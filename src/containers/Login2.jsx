c8f321921cd16045e1ca06ab27aaa50061d45c45
Ulises <ulises.rogo@gmail.com>
2020-04-08 02:43:05 -0500

    Añade contenido html y estilos a la sección de login
    Se añadieron los contenidos html y estilos correspondientes para
    la nueva sección de login.

--- src/containers/Login2.jsx
+++ src/containers/Login2.jsx
@@ -2,74 +2,52 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { loginRequest } from '../actions';
-import Header from '../components/Header';
+// import Header from '../components/Header';
+import Header2 from '../components/Header2';
 import '../assets/styles/components/Login.scss';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 
-const Login2 = props => {
+const Login2 = (props) => {
   const [form, setValues] = useState({
     email: '',
   });
 
-  const handleInput = event => {
+  const handleInput = (event) => {
     setValues({
       ...form,
-      [event.target.name]: event.target.value
-    })
-  }
+      [event.target.name]: event.target.value,
+    });
+  };
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     props.loginRequest(form);
     props.history.push('/');
-  }
+  };
 
   return (
     <>
-      <Header isLogin />
-      <h1>Company</h1>
-      <section className="login">
-        <section className="login__container">
-          <h2>Inicia sesión</h2>
-          <form className="login__container--form" onSubmit={handleSubmit}>
-            <input
-              name="email"
-              className="input"
-              type="text"
-              placeholder="Correo"
-              onChange={handleInput}
-            />
-            <input
-              name="password"
-              className="input"
-              type="password"
-              placeholder="Contraseña"
-              onChange={handleInput}
-            />
-            <button className="button">Iniciar sesión</button>
-            <div className="login__container--remember-me">
-              <label>
-                <input type="checkbox" id="cbox1" value="first_checkbox" /> Recuérdame
-              </label>
-              <a href="/">Olvidé mi contraseña</a>
-            </div>
+      <Header2 isLogin />
+      <main className='main login'>
+        <div className='login__card'>
+          <h1 className='centered'>Login</h1>
+          <form action=''>
+            <label htmlFor='username'>
+              Username
+              <input id='username' type='text' />
+            </label>
+            <label htmlFor='password'>
+              Password
+              <input id='password' type='password' />
+            </label>
+            <button className='button' type='button'>Enter</button>
           </form>
-          <section className="login__container--social-media">
-            <div><img src={googleIcon} /> Inicia sesión con Google</div>
-            <div><img src={twitterIcon} /> Inicia sesión con Twitter</div>
-          </section>
-          <p className="login__container--register">
-            No tienes ninguna cuenta {' '}
-            <Link to="/register">
-              Regístrate
-              </Link>
-          </p>
-        </section>
-      </section>
+        </div>
+      </main>
     </>
   );
-}
+};
 
 const mapDispatchToProps = {
   loginRequest,
