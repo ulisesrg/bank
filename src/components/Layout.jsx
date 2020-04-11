0f4236c55f118a84be61fc6a1344d8806811ed2c
Ulises <ulises.rogo@gmail.com>
2020-04-10 20:11:09 -0500

    Elimina archivos y código innecesario

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
 
 const Layout = ({ children }) => (
   <div className="App">
     {children}
-    {/* <Footer /> */}
   </div>
 );
 
