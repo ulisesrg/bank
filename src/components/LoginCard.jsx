a76190fbdbe1c004f724f9f2533a0cadde9121f8
Ulises <ulises.rogo@gmail.com>
2020-04-19 20:08:45 -0500

    Cambia la validación del formulario de login a través del state
    Se adaptó la validación para que actualice los valores de los
    inputs al state y genere, según condiciones, mensajes de error
    que también pasarán al state del formulario. Según los valores
    del estado, se desbloqueará el botón del formulario. Queda
    pendiente refactorizar el código.

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -6,174 +6,130 @@ import '../assets/styles/components/LoginCard.css';
 
 const LoginCard = (props) => {
   const [form, setValues] = useState({
-    username: '',
+    username: { value: '', error: '' },
+    password: { value: '', error: '' },
   });
 
-  let validUsername = false;
-  let validPassword = false;
+  console.log('render');
 
-  function validateEmpty(event, container) {
-    if (document.querySelector('.empty')) {
-      document.querySelector('.empty').remove();
-    }
-    if (event.target.value.length === 0) {
-      const node = document.createElement('p');
-      node.setAttribute('class', 'empty');
-      const textnode = document.createTextNode('Write an username');
-      node.appendChild(textnode);
-      container.appendChild(node);
-      return false;
-    }
-    return true;
-  }
-
-  function validateCharsLength(event, container) {
-    if (document.querySelector('.length')) {
-      document.querySelector('.length').remove();
-    }
-    if (
-      (event.target.value.length > 0 && event.target.value.length < 8) ||
-      event.target.value.length > 20
-    ) {
-      const node = document.createElement('p');
-      node.setAttribute('class', 'length');
-      const textnode = document.createTextNode(
-        'Username length should be between 8 and 20 characters'
-      );
-      node.appendChild(textnode);
-      container.appendChild(node);
-      return false;
-    }
-    return true;
-  }
-
-  function validateSpecialChars(event, container) {
-    const specialChars = /[^a-zA-Z0-9ñÑ!"$%&/]/g;
-    if (document.querySelector('.char')) {
-      document.querySelector('.char').remove();
-    }
-    if (event.target.value.match(specialChars)) {
-      const node = document.createElement('p');
-      node.setAttribute('class', 'char');
-      const textnode = document.createTextNode(
-        'Characters allowed: letters, numbers, !, ", $, %, &, /'
-      );
-      node.appendChild(textnode);
-      container.appendChild(node);
-      return false;
-    }
-    return true;
-  }
-
-  function validateEmptyPass(event, container) {
-    if (document.querySelector('.empty-p')) {
-      document.querySelector('.empty-p').remove();
-    }
-    if (event.target.value.length === 0) {
-      const node = document.createElement('p');
-      node.setAttribute('class', 'empty-p');
-      const textnode = document.createTextNode('Write a password');
-      node.appendChild(textnode);
-      container.appendChild(node);
-      return false;
-    }
-    return true;
-  }
-
-  function validateCharsLengthPass(event, container) {
-    if (document.querySelector('.length-p')) {
-      document.querySelector('.length-p').remove();
-    }
-    if (
-      (event.target.value.length > 0 && event.target.value.length < 8) ||
-      event.target.value.length > 20
-    ) {
-      const node = document.createElement('p');
-      node.setAttribute('class', 'length-p');
-      const textnode = document.createTextNode(
-        'Password length should be between 8 and 20 characters'
-      );
-      node.appendChild(textnode);
-      container.appendChild(node);
-      return false;
-    }
-    return true;
-  }
-
-  function validateSpecialCharsPass(event, container) {
-    const specialChars = /[^a-zA-Z0-9ñÑ!"$%&/]/g;
-    if (document.querySelector('.char-p')) {
-      document.querySelector('.char-p').remove();
-    }
-    if (event.target.value.match(specialChars)) {
-      const node = document.createElement('p');
-      node.setAttribute('class', 'char-p');
-      const textnode = document.createTextNode(
-        'Special characters accepted ! " $ % & /'
-      );
-      node.appendChild(textnode);
-      container.appendChild(node);
-      return false;
-    }
-    return true;
-  }
-
-  function validateMustContainCharsPass(event, container) {
-    const specialChars = /(?=.*[a-z])(?=.*[A-Z])(?=.*[!"$%&/])(?!.*(01|12|23|34|45|56|67|78|89))/g;
-    if (document.querySelector('.must-p')) {
-      document.querySelector('.must-p').remove();
-    }
-    if (
-      event.target.value.length > 0 &&
-      !specialChars.test(event.target.value)
-    ) {
-      const node = document.createElement('p');
-      node.setAttribute('class', 'must-p');
-      const textnode = document.createTextNode(
-        'Password must contain 1 lowercase, 1 uppercase and 1 special character. No sequence of number allowed, e.g. "23"'
-      );
-      node.appendChild(textnode);
-      container.appendChild(node);
-      return false;
-    }
-    return true;
-  }
-
-  const handleValidation = () => {
-    const $button = document.querySelector('button');
-    console.log(validUsername, validPassword);
-    if (validUsername && validPassword) {
-      $button.disabled = false;
-    } else {
-      $button.disabled = true;
-    }
+  const validForm = () => {
+    const { username, password } = form;
+    return (
+      username.value && password.value && !username.error && !password.error
+    );
   };
 
   const handleUserInput = (event) => {
-    // setValues({
-    //   ...form,
-    //   [event.target.name]: event.target.value,
-    // });
-    const $usernameLabel = document.getElementById('username-label');
-    const validEmpty = validateEmpty(event, $usernameLabel);
-    const validLength = validateCharsLength(event, $usernameLabel);
-    const validSpecial = validateSpecialChars(event, $usernameLabel);
-    validUsername = validEmpty && validLength && validSpecial;
-    handleValidation();
+    const notAllowedChars = /[^a-zA-Z0-9ñÑ!"$%&/]/g;
+    if (!event.target.value) {
+      setValues({
+        ...form,
+        username: {
+          ...form.username,
+          value: event.target.value,
+          error: 'Write an unsername',
+        },
+      });
+      // console.log(2);
+    } else if (
+      (event.target.value.length > 0 && event.target.value.length < 8) ||
+      event.target.value.length > 20
+    ) {
+      setValues({
+        ...form,
+        username: {
+          ...form.username,
+          value: event.target.value,
+          error: 'Username length should be between 8 and 20 characters',
+        },
+      });
+      // console.log(3);
+    } else if (event.target.value.match(notAllowedChars)) {
+      setValues({
+        ...form,
+        username: {
+          ...form.username,
+          value: event.target.value,
+          error: 'Characters allowed: letters, numbers, !, ", $, %, &, /',
+        },
+      });
+      // console.log(4);
+    } else {
+      setValues({
+        ...form,
+        username: {
+          ...form.username,
+          value: event.target.value,
+          error: '',
+        },
+      });
+      // console.log(1);
+    }
   };
 
   const handlePasswordInput = (event) => {
-    const $passwordLabel = document.getElementById('password-label');
-    const validEmpty = validateEmptyPass(event, $passwordLabel);
-    const validLength = validateCharsLengthPass(event, $passwordLabel);
-    const validSpecial = validateSpecialCharsPass(event, $passwordLabel);
-    const validMust = validateMustContainCharsPass(event, $passwordLabel);
-    validPassword = validEmpty && validLength && validSpecial && validMust;
-    handleValidation();
+    const notAllowedChars = /[^a-zA-Z0-9ñÑ!"$%&/]/g;
+    const necessaryChars = /(?=.*[a-z])(?=.*[A-Z])(?=.*[!"$%&/])(?!.*(01|12|23|34|45|56|67|78|89))/g;
+    if (!event.target.value) {
+      setValues({
+        ...form,
+        password: {
+          ...form.password,
+          value: event.target.value,
+          error: 'Write an unsername',
+        },
+      });
+      // console.log(2);
+    } else if (
+      (event.target.value.length > 0 && event.target.value.length < 8) ||
+      event.target.value.length > 20
+    ) {
+      setValues({
+        ...form,
+        password: {
+          ...form.password,
+          value: event.target.value,
+          error: 'Password length should be between 8 and 20 characters',
+        },
+      });
+      // console.log(3);
+    } else if (event.target.value.match(notAllowedChars)) {
+      setValues({
+        ...form,
+        password: {
+          ...form.password,
+          value: event.target.value,
+          error: 'Characters allowed: letters, numbers, !, ", $, %, &, /',
+        },
+      });
+      // console.log(4);
+    } else if (!necessaryChars.test(event.target.value)) {
+      setValues({
+        ...form,
+        password: {
+          ...form.password,
+          value: event.target.value,
+          error:
+            'Password must contain 1 lowercase, 1 uppercase and 1 special character. No sequence of number allowed, e.g. "23"',
+        },
+      });
+      // console.log(4);
+    } else {
+      setValues({
+        ...form,
+        password: {
+          ...form.password,
+          value: event.target.value,
+          error: '',
+        },
+      });
+      // console.log(1);
+    }
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    // handleValidation();
     // props.loginRequest(form);
     props.history.push('/home');
   };
@@ -189,8 +145,12 @@ const LoginCard = (props) => {
             id='username'
             type='text'
             onChange={handleUserInput}
+            value={form.username.value}
           />
         </label>
+        {form.username.error && (
+          <span className='error'>{form.username.error}</span>
+        )}
         <label htmlFor='password' id='password-label'>
           Password
           <input
@@ -200,14 +160,22 @@ const LoginCard = (props) => {
             onChange={handlePasswordInput}
           />
         </label>
-        <button className='button' type='submit' disabled>
-          Enter
-        </button>
+        {form.password.error && (
+          <span className='error'>{form.password.error}</span>
+        )}
+        {validForm() ? (
+          <button className='button' type='submit'>
+            Enter
+          </button>
+        ) : (
+          <button className='button' type='submit' disabled>
+            Enter
+          </button>
+        )}
       </form>
     </div>
   );
 };
-
 const mapDispatchToProps = {
   loginRequest,
 };
